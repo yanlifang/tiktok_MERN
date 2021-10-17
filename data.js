9652584bed7b1ac5f3f5f1f0978de4c03c306c09
Lifang Yan <lifang.yan.cs@gmail.com>
2021-10-16 20:29:36 -0500

    complete backend

---
+++ data.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+        url: "https://www.youtube.com/watch?v=RBsu_dfAIh0&list=RDRBsu_dfAIh0&start_radio=1",
+        channel: "youtube-legend of fei",
+        song: "XIWEI",
+        likes: "125463",
+        messages: "15235",
+        description: "one of song in legend of fei, singer is yibo wang",
+        shares: "500",
+
+    },
+    {
+        url: "https://www.youtube.com/watch?v=O3J3VzZphEQ&list=RDMM&start_radio=1&rv=RBsu_dfAIh0",
+        channel: "Kpop",
+        song: "Starlight of moutains and rivers",
+        likes: "565262",
+        messages: "1535",
+        description: "Created for Oct 1, 2021, singer is yibo wang",
+        shares: "500",
+    }
+]
+
+
+
+export default data; 
---
+++ server.js
@@ -0,0 +1,71 @@
+import express from "express";
+import mongoose from 'mongoose';
+import data from  './data.js';
+import Videos from './dbModel.js';
+
+
+
+
+//app config
+const app = express();
+const port = 8100;
+
+//middlewares 
+app.use(express.json());
+
+//db config 
+
+const connection_url = 'mongodb+srv://admin:GQo4DQ8TnVof0wiM@cluster0.5lc1d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(connection_url, 
+    async(err)=>{
+        if(err) throw err;
+        console.log("connected to db")
+    }
+);
+
+
+//api endpoints 
+
+
+app.get('/', (req, res) => res.status(200).send('HELLO WORLD'));
+
+
+app.get('/v1/posts', (req, res) => res.status(200).send(data) );
+
+app.get('/v2/posts', (req, res)=>{
+
+    Videos.find((err, data)=>{
+        if(err)
+        {
+
+        res.status(500).send(err);
+        }
+        else
+        {
+            res.status(200).send(data);
+        }
+    });
+});
+
+
+app.post('/v2/posts', (req, res)=> {
+    //POST request is to ADD DATA to the database
+    //IT will let us add a video to the videos COLLECTION 
+    const dbVideos = req.body;
+
+    Videos.create(dbVideos, (err, data)=>{
+        if (err)
+        {
+            res.status(500).send(err)
+        }
+        else 
+        {
+            res.status(201).send(data)
+        }
+    })
+})
+
+//listen 
+app.listen(port, ()=>console.log(`listening on localhost: ${port}`));
+
